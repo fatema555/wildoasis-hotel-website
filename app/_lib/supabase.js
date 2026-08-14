9d345760f7bd580cb384206620791e5fccd5968d
fatema555 <fatema555ge@gmail.com>
2026-08-15 02:35:39 +0300

    fix supabase build

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -7,10 +7,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-// استخدام القيم الموجودة أو قيم افتراضية مؤقتة لمنع توقف الـ Build
 const supabaseUrl =
-  process.env.SUPABASE_URL || "https://algzjykvaiwhdwguiss.supabase.co";
-const supabaseKey =
-  process.env.SUPABASE_PUBLISHABLE_KEY || "dummy_key_for_build";
+  process.env.SUPABASE_URL || "https://placeholder-url.supabase.co";
+const supabaseKey = process.env.SUPABASE_PUBLISHABLE_KEY || "placeholder-key";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
